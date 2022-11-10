48333d92972d7d97d50b46f08cbde1ecc79a67a9
[claks HDS] <you@example.com>
2022-11-10 13:40:14 +0000

    Footer component

---
+++ src/assets/wrappers/footer.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+const Wrapper = styled.footer`
+  height: 3vh;
+  display: grid;
+  padding-top: 0.55rem;
+  place-items: center;
+  .social-links {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    justify-items: center;
+    gap: 1rem;
+    a {
+      font-size: 1.4rem;
+      margin: 0 1rem;
+      color: var(--textColor);
+      transition: var(--transition);
+    }
+    a:hover {
+      font-size: 1.6rem;
+      color: var(--headlineColor);
+    }
+  }
+  .logo {
+    font-family: var(--headlineFont);
+    font-size: 2rem;
+  }
+`;
+
+export default Wrapper;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,31 @@
 import React from "react";
+import logo from "../assets/images/logo.svg";
+import { socials } from "../utils/data";
+/* Styles */
+import Wrapper from "../assets/wrappers/footer";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Wrapper>
+      <ul className='social-links'>
+        {socials.map((socialLink) => {
+          const { id, icon, url } = socialLink;
+          return (
+            <li key={id}>
+              <a href={url} target='_blank'>
+                {icon}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p>
+        &copy; <span className='logo'>clarisse hd</span> - all rights reserved{" "}
+        {""}
+        {new Date().getFullYear()}
+      </p>
+    </Wrapper>
+  );
 };
 
 export default Footer;
